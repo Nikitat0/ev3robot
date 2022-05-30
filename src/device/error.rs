958050f92f553f5f36ce6cfe3e3f4256c1ec40b1
Nikitat0 <nikitatzero@yandex.ru>
2022-05-30 16:57:44 +0700

    Mark device::ConnectionError non_exhaustive

--- src/device/error.rs
+++ src/device/error.rs
@@ -3,6 +3,7 @@ use thiserror::Error;
 pub type ConnectionResult<T> = Result<T, ConnectionError>;
 
 #[derive(Debug, Error)]
+#[non_exhaustive]
 pub enum ConnectionError {
     #[error("There is no device with name {name}")]
     NotExist { name: String },
