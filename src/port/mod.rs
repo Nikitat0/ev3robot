34df51a2424ce8b7725de71a20088eca6a1edbf7
Nikitat0 <nikitatzero@yandex.ru>
2022-05-31 12:27:33 +0700

    Add Port

---
+++ src/port/mod.rs
@@ -0,0 +1,62 @@
+use std::convert::Infallible;
+use std::fmt::{self, Debug, Display, Formatter};
+use std::result::Result as StdResult;
+use std::str::FromStr;
+use std::sync::Arc;
+
+#[derive(Clone, Eq)]
+pub enum Port {
+    Static(&'static str),
+    Dynamic(Arc<str>),
+}
+
+impl Port {
+    pub fn new<T: Into<Port>>(name: T) -> Self {
+        name.into()
+    }
+
+    pub const fn new_static(name: &'static str) -> Port {
+        Port::Static(name)
+    }
+}
+
+impl<T: Into<Arc<str>>> From<T> for Port {
+    fn from(v: T) -> Self {
+        Port::Dynamic(v.into())
+    }
+}
+
+impl FromStr for Port {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> StdResult<Self, Self::Err> {
+        Ok(s.into())
+    }
+}
+
+impl PartialEq for Port {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_ref() == other.as_ref()
+    }
+}
+
+impl AsRef<str> for Port {
+    fn as_ref(&self) -> &str {
+        match self {
+            Port::Static(port) => port,
+            Port::Dynamic(port) => port,
+        }
+    }
+}
+
+impl Debug for Port {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Port").field(&self.as_ref()).finish()
+    }
+}
+
+impl Display for Port {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.write_str(self.as_ref())
+    }
+}
