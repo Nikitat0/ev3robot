bd71f5143a7d654763f0594100257b8f612f42d5
Nikitat0 <nikitatzero@yandex.ru>
2022-05-31 10:07:06 +0700

    Add ConnectionError::Unexpected constructors

--- src/device/error.rs
+++ src/device/error.rs
@@ -1,3 +1,6 @@
+use std::error;
+use std::fmt::Display;
+
 use thiserror::Error;
 
 pub type ConnectionResult<T> = Result<T, ConnectionError>;
@@ -9,4 +12,24 @@ pub enum ConnectionError {
     NotExist { name: String },
     #[error(transparent)]
     Unexpected(#[from] anyhow::Error),
+}
+
+impl ConnectionError {
+    pub fn new_unexpected<E>(err: E) -> ConnectionError
+    where
+        E: error::Error + Send + Sync + 'static,
+    {
+        ConnectionError::Unexpected(anyhow::Error::new(err))
+    }
+
+    pub fn new_unexpected_with_context<E, C>(
+        err: E,
+        context: C,
+    ) -> ConnectionError
+    where
+        E: error::Error + Send + Sync + 'static,
+        C: Display + Send + Sync + 'static,
+    {
+        ConnectionError::Unexpected(anyhow::Error::new(err).context(context))
+    }
 }
