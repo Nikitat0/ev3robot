6b8373dd024f74b5a8aab6a58700960295eb34ee
Nikitat0 <nikitatzero@yandex.ru>
2022-06-02 14:28:11 +0700

    Add TachoMotorDevice

---
+++ src/ev3dev/tacho_motor/mod.rs
@@ -0,0 +1,15 @@
+use crate as ev3robot;
+use crate::device::{ReadOnlyAttributeFile, ReadWriteAttributeFile};
+
+#[derive(Debug, Device)]
+#[ev3robot(class = "tacho-motor")]
+pub struct TachoMotorDevice {
+    pub count_per_rot: u32,
+    pub duty_cycle: ReadOnlyAttributeFile<i8>,
+    pub duty_cycle_sp: ReadWriteAttributeFile<i8>,
+    pub position: ReadWriteAttributeFile<i32>,
+    pub position_sp: ReadWriteAttributeFile<i32>,
+    pub max_speed: u32,
+    pub speed: ReadOnlyAttributeFile<i32>,
+    pub speed_sp: ReadWriteAttributeFile<i32>,
+}
