fb0afffcaf10598e12f6469b281df42b65e67015
Nikitat0 <nikitatzero@yandex.ru>
2022-06-10 11:04:34 +0700

    Add trait FindableDevice

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,9 +1,13 @@
 mod attribute;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub use attribute::*;
 
 pub trait Device: Sized {
     fn open<P: AsRef<Path>>(device_node: P) -> anyhow::Result<Self>;
+}
+
+pub trait FindableDevice {
+    fn find_device_nodes() -> Vec<PathBuf>;
 }
