0851cd6a38936151ece26229d0d8aab306dc9028
Nikitat0 <nikitatzero@yandex.ru>
2022-06-10 16:23:38 +0700

    Add derive(FindableDevice)

---
+++ macros/src/derive_findable_device_macro.rs
@@ -0,0 +1,63 @@
+use darling::FromDeriveInput;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::parse_quote;
+
+pub fn derive_findable_device_impl(raw_input: TokenStream2) -> TokenStream2 {
+    let input = match syn::parse2(raw_input) {
+        Ok(input) => input,
+        Err(err) => return err.into_compile_error(),
+    };
+    let device_struct = match FindableDeviceStruct::from_derive_input(&input) {
+        Ok(device_impl) => device_impl,
+        Err(err) => return err.write_errors(),
+    };
+    let device_impl = device_struct.gen_impl();
+
+    quote! { #device_impl }
+}
+
+#[derive(FromDeriveInput)]
+#[darling(attributes(findable_device))]
+struct FindableDeviceStruct {
+    ident: syn::Ident,
+    generics: syn::Generics,
+    class: String,
+    driver: Option<String>,
+}
+
+impl FindableDeviceStruct {
+    fn gen_impl(&self) -> TokenStream2 {
+        let Self { ident, generics, class, driver } = self;
+        let (impl_generics, ty_generics, where_clause) =
+            generics.split_for_impl();
+        let filter_by_driver: Option<syn::Stmt> =
+            driver.as_ref().map(|driver| {
+                parse_quote! {
+                    let device_nodes =
+                        device_nodes .filter(|node| {
+                            ev3robot::device::utils::device_node_driver_name(
+                                node
+                            ).map(|it| it == #driver).unwrap_or_default()
+                        });
+                }
+            });
+
+        quote! {
+            impl #impl_generics ev3robot::device::FindableDevice
+                for #ident #ty_generics #where_clause
+            {
+                fn find_device_nodes() -> ::std::vec::Vec<::std::path::PathBuf>
+                {
+                    use ::std::iter::Iterator;
+                    let device_nodes =
+                        ev3robot::device::utils::find_device_nodes_by_class(
+                            #class
+                        ).into_iter();
+                    #filter_by_driver
+                    device_nodes.collect()
+                }
+            }
+        }
+    }
+}
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -4,8 +4,15 @@ extern crate darling;
 use proc_macro::TokenStream;
 
 mod derive_device_macro;
+mod derive_findable_device_macro;
 
 #[proc_macro_derive(Device, attributes(ev3robot))]
 pub fn derive_device(input: TokenStream) -> TokenStream {
     derive_device_macro::derive_device_impl(input.into()).into()
+}
+
+#[proc_macro_derive(FindableDevice, attributes(findable_device))]
+pub fn derive_findable_device(input: TokenStream) -> TokenStream {
+    derive_findable_device_macro::derive_findable_device_impl(input.into())
+        .into()
 }
