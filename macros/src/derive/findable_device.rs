9e8a3957e79e977dc587c1a2d7d261b41475e397
Nikitat0 <nikitatzero@yandex.ru>
2022-06-12 12:05:10 +0700

    Add impl of Findable and FindableBy<Port> by FindableDevice macro

--- macros/src/derive/findable_device.rs
+++ macros/src/derive/findable_device.rs
@@ -1,3 +1,6 @@
+use std::ops::Not;
+
+use darling::util::Flag;
 use darling::FromDeriveInput;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
@@ -24,11 +27,20 @@ struct FindableDeviceStruct {
     generics: syn::Generics,
     class: String,
     driver: Option<String>,
+    no_findable_impl: Flag,
+    no_findable_by_port_impl: Flag,
 }
 
 impl FindableDeviceStruct {
     fn gen_impl(&self) -> TokenStream2 {
-        let Self { ident, generics, class, driver } = self;
+        let Self {
+            ident,
+            generics,
+            class,
+            driver,
+            no_findable_impl,
+            no_findable_by_port_impl,
+        } = self;
         let (impl_generics, ty_generics, where_clause) =
             generics.split_for_impl();
         let filter_by_driver: Option<syn::Stmt> =
@@ -40,6 +52,59 @@ impl FindableDeviceStruct {
                                 node
                             ).map(|it| it == #driver).unwrap_or_default()
                         });
+                }
+            });
+
+        let findable_impl = no_findable_impl.is_present().not().then(|| {
+            quote! {
+                impl #impl_generics crate::find::Findable
+                    for #ident #ty_generics #where_clause
+                {
+                    fn find() -> crate::find::Result<Self> {
+                        use crate::device::Device as _;
+                        use crate::device::FindableDevice as _;
+                        let device_node = crate::find::find_in(
+                            Self::find_device_nodes()
+                        )?;
+                        Ok(Self::open(device_node)?)
+                    }
+                }
+            }
+        });
+
+        let findable_by_port_impl =
+            no_findable_by_port_impl.is_present().not().then(|| {
+                quote! {
+                    impl #impl_generics
+                        crate::find::FindableBy<crate::port::Port>
+                        for #ident #ty_generics #where_clause
+                    {
+                        fn find_by<U>(value: U) -> crate::find::Result<Self>
+                        where
+                            U: ::std::borrow::Borrow<crate::port::Port>
+                        {
+                            fn find_by_port(
+                                port: &crate::port::Port
+                            ) -> crate::find::Result<#ident> {
+                                use crate::device::Device as _;
+                                use crate::device::FindableDevice as _;
+                                use crate::device::utils::*;
+                                let device_node = crate::find::find_in(
+                                    #ident::find_device_nodes()
+                                        .into_iter()
+                                        .filter(|device_node|{
+                                            device_node_port(device_node)
+                                                .map(|it|{
+                                                    &it == port
+                                                })
+                                                .unwrap_or_default()
+                                        })
+                                )?;
+                                Ok(#ident::open(device_node)?)
+                            }
+                            find_by_port(::std::borrow::Borrow::borrow(&value))
+                        }
+                    }
                 }
             });
 
@@ -58,6 +123,9 @@ impl FindableDeviceStruct {
                     device_nodes.collect()
                 }
             }
+
+            #findable_impl
+            #findable_by_port_impl
         }
     }
 }
