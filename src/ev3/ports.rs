3f33ad47c47a8a47d28c5cbd3820bd34e4ffd0ec
Nikitat0 <nikitatzero@yandex.ru>
2022-06-12 14:15:25 +0700

    Add ev3::ports

---
+++ src/ev3/ports.rs
@@ -0,0 +1,11 @@
+use crate::port::port;
+
+port!(IN_1, "ev3-ports:in1");
+port!(IN_2, "ev3-ports:in2");
+port!(IN_3, "ev3-ports:in3");
+port!(IN_4, "ev3-ports:in4");
+
+port!(OUT_A, "ev3-ports:outA");
+port!(OUT_B, "ev3-ports:outB");
+port!(OUT_C, "ev3-ports:outC");
+port!(OUT_D, "ev3-ports:outD");
