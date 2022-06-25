84a8df123af8432621293e7559e3eb3c5bf6a618
Nikitat0 <nikitatzero@yandex.ru>
2022-06-25 11:20:09 +0700

    Remove impl TachoMotor

--- src/motor/tacho/mod.rs
+++ src/motor/tacho/mod.rs
@@ -1,14 +1,11 @@
 mod command;
 mod state;
 mod stop_action;
-use std::io;
 
 pub use command::*;
 pub use state::*;
 pub use stop_action::*;
 
-use super::duty_cycle::*;
-use super::polarity::*;
 use crate::device::{
     ReadOnlyAttributeFile, ReadWriteAttributeFile, WriteOnlyAttributeFile,
 };
@@ -28,84 +25,4 @@ pub struct TachoMotor {
     speed: ReadOnlyAttributeFile,
     speed_sp: ReadWriteAttributeFile,
     stop_action: ReadWriteAttributeFile,
-}
-
-impl TachoMotor {
-    pub fn command(&mut self, command: Command) -> io::Result<()> {
-        self.command.set_value(command)
-    }
-
-    pub fn count_per_rot(&mut self) -> u32 {
-        self.count_per_rot
-    }
-
-    pub fn duty_cycle(&mut self) -> io::Result<DutyCycle> {
-        self.duty_cycle.value()
-    }
-
-    pub fn duty_cycle_sp(&mut self) -> io::Result<DutyCycle> {
-        self.duty_cycle_sp.value()
-    }
-
-    pub fn set_duty_cycle_sp(
-        &mut self,
-        duty_cycle: DutyCycle,
-    ) -> io::Result<()> {
-        self.duty_cycle_sp.set_value(duty_cycle)
-    }
-
-    pub fn polarity(&mut self) -> io::Result<Polarity> {
-        self.polarity.value()
-    }
-
-    pub fn set_polarity(&mut self, polarity: Polarity) -> io::Result<()> {
-        self.polarity.set_value(polarity)
-    }
-
-    pub fn position(&mut self) -> io::Result<i32> {
-        self.position.value()
-    }
-
-    pub fn set_position(&mut self, pos: i32) -> io::Result<()> {
-        self.position.set_value(pos)
-    }
-
-    pub fn position_sp(&mut self) -> io::Result<i32> {
-        self.position_sp.value()
-    }
-
-    pub fn set_position_sp(&mut self, pos: i32) -> io::Result<()> {
-        self.position_sp.set_value(pos)
-    }
-
-    pub fn state(&mut self) -> io::Result<State> {
-        self.state.value()
-    }
-
-    pub fn max_speed(&mut self) -> u32 {
-        self.max_speed
-    }
-
-    pub fn speed(&mut self) -> io::Result<i32> {
-        self.speed.value()
-    }
-
-    pub fn speed_sp(&mut self) -> io::Result<i32> {
-        self.speed_sp.value()
-    }
-
-    pub fn set_speed_sp(&mut self, speed: i32) -> io::Result<()> {
-        self.speed_sp.set_value(speed)
-    }
-
-    pub fn stop_action(&mut self) -> io::Result<StopAction> {
-        self.stop_action.value()
-    }
-
-    pub fn set_stop_action(
-        &mut self,
-        stop_action: StopAction,
-    ) -> io::Result<()> {
-        self.stop_action.set_value(stop_action)
-    }
 }
