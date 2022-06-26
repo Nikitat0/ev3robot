390b883dcb3bbba09a0ae76d20adb72bb4689789
Nikitat0 <nikitatzero@yandex.ru>
2022-06-26 12:48:42 +0700

    Clear derive(FindableDevice) code

--- macros/src/derive/findable_device.rs
+++ macros/src/derive/findable_device.rs
@@ -1,8 +1,6 @@
-use std::ops::Not;
-
 use darling::util::Flag;
 use darling::{FromDeriveInput, ToTokens};
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::TokenStream;
 use quote::quote;
 use syn::parse_quote;
 
@@ -17,16 +15,9 @@ pub struct FindableDevice {
     no_findable_by_port_impl: Flag,
 }
 
-impl ToTokens for FindableDevice {
-    fn to_tokens(&self, tokens: &mut TokenStream2) {
-        let Self {
-            ident,
-            generics,
-            class,
-            driver,
-            no_findable_impl,
-            no_findable_by_port_impl,
-        } = self;
+impl FindableDevice {
+    fn findable_device_impl(&self) -> TokenStream {
+        let Self { ident, generics, class, driver, .. } = self;
         let (impl_generics, ty_generics, where_clause) =
             generics.split_for_impl();
         let filter_by_driver: Option<syn::Stmt> =
@@ -40,61 +31,7 @@ impl ToTokens for FindableDevice {
                         });
                 }
             });
-
-        let findable_impl = no_findable_impl.is_present().not().then(|| {
-            quote! {
-                impl #impl_generics crate::find::Findable
-                    for #ident #ty_generics #where_clause
-                {
-                    fn find() -> crate::find::Result<Self> {
-                        use crate::device::Device as _;
-                        use crate::device::FindableDevice as _;
-                        let device_node = crate::find::find_in(
-                            Self::find_device_nodes()
-                        )?;
-                        Ok(Self::open(device_node)?)
-                    }
-                }
-            }
-        });
-
-        let findable_by_port_impl =
-            no_findable_by_port_impl.is_present().not().then(|| {
-                quote! {
-                    impl #impl_generics
-                        crate::find::FindableBy<crate::port::Port>
-                        for #ident #ty_generics #where_clause
-                    {
-                        fn find_by<U>(value: U) -> crate::find::Result<Self>
-                        where
-                            U: ::std::borrow::Borrow<crate::port::Port>
-                        {
-                            fn find_by_port(
-                                port: &crate::port::Port
-                            ) -> crate::find::Result<#ident> {
-                                use crate::device::Device as _;
-                                use crate::device::FindableDevice as _;
-                                use crate::device::utils::*;
-                                let device_node = crate::find::find_in(
-                                    #ident::find_device_nodes()
-                                        .into_iter()
-                                        .filter(|device_node|{
-                                            device_node_port(device_node)
-                                                .map(|it|{
-                                                    &it == port
-                                                })
-                                                .unwrap_or_default()
-                                        })
-                                )?;
-                                Ok(#ident::open(device_node)?)
-                            }
-                            find_by_port(::std::borrow::Borrow::borrow(&value))
-                        }
-                    }
-                }
-            });
-
-        tokens.extend(quote! {
+        quote! {
             impl #impl_generics crate::device::FindableDevice
                 for #ident #ty_generics #where_clause
             {
@@ -109,7 +46,76 @@ impl ToTokens for FindableDevice {
                     device_nodes.collect()
                 }
             }
+        }
+    }
 
+    fn findable_impl(&self) -> Option<TokenStream> {
+        let Self { ident, generics, no_findable_impl, .. } = self;
+        if no_findable_impl.is_present() {
+            return None;
+        }
+        let (impl_generics, ty_generics, where_clause) =
+            generics.split_for_impl();
+        Some(quote! {
+            impl #impl_generics crate::find::Findable
+                for #ident #ty_generics #where_clause
+            {
+                fn find() -> crate::find::Result<Self> {
+                    use crate::device::Device as _;
+                    use crate::device::FindableDevice as _;
+                    let device_node = crate::find::find_in(
+                        Self::find_device_nodes()
+                    )?;
+                    Ok(Self::open(device_node)?)
+                }
+            }
+        })
+    }
+
+    fn findable_by_port_impl(&self) -> Option<TokenStream> {
+        let Self { ident, generics, no_findable_by_port_impl, .. } = self;
+        if no_findable_by_port_impl.is_present() {
+            return None;
+        }
+        let (impl_generics, ty_generics, where_clause) =
+            generics.split_for_impl();
+        Some(quote! {
+            impl #impl_generics crate::find::FindableBy<crate::port::Port>
+                for #ident #ty_generics #where_clause
+            {
+                fn find_by<U>(port: U) -> crate::find::Result<Self>
+                where
+                    U: ::std::borrow::Borrow<crate::port::Port>
+                {
+                    use crate::device::Device as _;
+                    use crate::device::FindableDevice as _;
+                    use crate::device::utils::*;
+                    let port = ::std::borrow::Borrow::borrow(&port);
+                    let device_node = crate::find::find_in(
+                        #ident::find_device_nodes()
+                            .into_iter()
+                            .filter(|device_node|{
+                                device_node_port(device_node)
+                                    .map(|it|{
+                                        &it == port
+                                    })
+                                    .unwrap_or_default()
+                            })
+                    )?;
+                    Ok(#ident::open(device_node)?)
+                }
+            }
+        })
+    }
+}
+
+impl ToTokens for FindableDevice {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let findable_device_impl = self.findable_device_impl();
+        let findable_impl = self.findable_impl();
+        let findable_by_port_impl = self.findable_by_port_impl();
+        tokens.extend(quote! {
+            #findable_device_impl
             #findable_impl
             #findable_by_port_impl
         })
