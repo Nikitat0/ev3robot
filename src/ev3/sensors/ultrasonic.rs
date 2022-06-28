35141f7aa43a8fdc7182dc9da634cfd3c4355573
Nikitat0 <nikitatzero@yandex.ru>
2022-06-28 12:51:42 +0700

    Add DistanceCmMode for UltrasonicSensor

--- src/ev3/sensors/ultrasonic.rs
+++ src/ev3/sensors/ultrasonic.rs
@@ -7,3 +7,22 @@ pub struct UltrasonicSensor {
     #[device(attr_name = "value0")]
     value: ReadOnlyAttributeFile,
 }
+
+impl UltrasonicSensor {
+    pub fn cm_distance_mode(&mut self) -> anyhow::Result<CmDistanceMode> {
+        self.mode.set_value("US-DIST-CM")?;
+        Ok(CmDistanceMode(self))
+    }
+}
+
+pub struct CmDistanceMode<'a>(&'a mut UltrasonicSensor);
+
+impl CmDistanceMode<'_> {
+    pub fn mm(&mut self) -> anyhow::Result<i32> {
+        Ok(self.0.value.value()?)
+    }
+
+    pub fn cm(&mut self) -> anyhow::Result<i32> {
+        self.mm().map(|it| it / 10)
+    }
+}
