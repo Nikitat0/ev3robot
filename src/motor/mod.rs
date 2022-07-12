1b918058a1bcdce17b2a6b8d4abb739baea13100
Nikitat0 <nikitatzero@yandex.ru>
2022-07-12 12:53:57 +0700

    Add motor::PerSecond<Units> speed type

--- src/motor/mod.rs
+++ src/motor/mod.rs
@@ -1,10 +1,12 @@
 mod duty_cycle;
 mod polarity;
 mod run;
+mod speed;
 mod stop;
 pub mod tacho;
 
 pub use duty_cycle::*;
 pub use polarity::*;
 pub use run::*;
+pub use speed::*;
 pub use stop::*;
