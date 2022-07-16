940e092111c55d534332db62b7c40c7b2a8e0fd8
Nikitat0 <nikitatzero@yandex.ru>
2022-07-16 14:23:22 +0700

    fixup! Add angular units

--- src/motor/tacho/units.rs
+++ src/motor/tacho/units.rs
@@ -3,7 +3,41 @@ use derive_more::*;
 macro_rules! unit {
     (
         $(#[$outer:meta])*
-        pub struct $name:ident($(#[$inner:meta])* $repr:ty);
+        pub struct $name:ident(i32);
+    ) => {
+        unit!{
+            BREAK_RECURSION
+            $(#[$outer])*
+            #[derive(Eq, Ord, Binary, Octal, LowerHex, UpperHex)]
+            pub struct $name(i32);
+        }
+
+        impl $name {
+            pub fn as_i32(&self) -> i32 {
+                self.0
+            }
+        }
+    };
+    (
+        $(#[$outer:meta])*
+        pub struct $name:ident(f32);
+    ) => {
+        unit!{
+            BREAK_RECURSION
+            $(#[$outer])*
+            pub struct $name(f32);
+        }
+
+        impl $name {
+            pub fn as_f32(&self) -> f32 {
+                self.0
+            }
+        }
+    };
+    (
+        $(BREAK_RECURSION)?
+        $(#[$outer:meta])*
+        pub struct $name:ident($repr:ty);
     ) => {
          $(#[$outer])*
          #[derive(
@@ -11,16 +45,10 @@ macro_rules! unit {
             Copy,
             Debug,
             PartialEq,
-            Eq,
             PartialOrd,
-            Ord,
             Constructor,
             From,
             Display,
-            Binary,
-            Octal,
-            LowerHex,
-            UpperHex,
             LowerExp,
             UpperExp,
         )]
@@ -41,5 +69,19 @@ unit! {
     pub struct Degrees(i32);
 }
 unit! {
-    pub struct Revolutions(i32);
+    pub struct Revolutions(f32);
+}
+
+const DEGREES_PER_REVOLUTION: f32 = 360.0;
+
+impl From<Degrees> for Revolutions {
+    fn from(degrees: Degrees) -> Self {
+        Revolutions::from(degrees.as_i32() as f32 / DEGREES_PER_REVOLUTION)
+    }
+}
+
+impl From<Revolutions> for Degrees {
+    fn from(revolutions: Revolutions) -> Self {
+        Degrees::from((revolutions.as_f32() * DEGREES_PER_REVOLUTION) as i32)
+    }
 }
