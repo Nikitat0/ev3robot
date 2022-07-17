986a2e05afe14951aad90a02d50314228eed4dcd
Nikitat0 <nikitatzero@yandex.ru>
2022-07-17 13:29:19 +0700

    Impl Run for TachoMotor

--- src/motor/tacho/mod.rs
+++ src/motor/tacho/mod.rs
@@ -16,13 +16,13 @@ use crate::device::{
 #[findable_device(class = "tacho-motor")]
 pub struct TachoMotor {
     command: WriteOnlyAttributeFile,
-    count_per_rot: u32,
+    count_per_rot: i32,
     duty_cycle: ReadOnlyAttributeFile,
     duty_cycle_sp: ReadWriteAttributeFile,
     polarity: ReadWriteAttributeFile,
     position: ReadWriteAttributeFile,
     position_sp: ReadWriteAttributeFile,
-    max_speed: u32,
+    max_speed: i32,
     state: ReadOnlyAttributeFile,
     speed: ReadOnlyAttributeFile,
     speed_sp: ReadWriteAttributeFile,
@@ -30,10 +30,49 @@ pub struct TachoMotor {
 }
 
 impl TachoMotor {
+    fn run(&mut self, speed: PerSecond<TachoCounts>) -> anyhow::Result<()> {
+        self.speed_sp.set_value(speed.units().as_i32())?;
+        self.command.set_value("run-forever")?;
+        Ok(())
+    }
+
     fn stop(&mut self, stop_action: String) -> anyhow::Result<()> {
         self.stop_action.set_value(stop_action)?;
         self.command.set_value("stop")?;
         Ok(())
+    }
+
+    fn degrees_to_tacho_counts(&self, degrees: Degrees) -> TachoCounts {
+        TachoCounts::new((self.count_per_rot * degrees.as_i32() / 360) as i32)
+    }
+
+    fn revolutions_to_tacho_counts(
+        &self,
+        revolutions: Revolutions,
+    ) -> TachoCounts {
+        TachoCounts::new(
+            (self.count_per_rot as f32 * revolutions.as_f32()) as i32,
+        )
+    }
+}
+
+impl Run<PerSecond<TachoCounts>> for TachoMotor {
+    fn run(&mut self, speed: PerSecond<TachoCounts>) -> anyhow::Result<()> {
+        self.run(speed)
+    }
+}
+
+impl Run<PerSecond<Degrees>> for TachoMotor {
+    fn run(&mut self, speed: PerSecond<Degrees>) -> anyhow::Result<()> {
+        self.run(PerSecond::new(self.degrees_to_tacho_counts(speed.units())))
+    }
+}
+
+impl Run<PerSecond<Revolutions>> for TachoMotor {
+    fn run(&mut self, speed: PerSecond<Revolutions>) -> anyhow::Result<()> {
+        self.run(PerSecond::new(
+            self.revolutions_to_tacho_counts(speed.units()),
+        ))
     }
 }
 
