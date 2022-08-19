c636625785c06150c66d2f8e450f07b5d3041f99
Nikitat0 <nikitatzero@yandex.ru>
2022-08-19 17:17:30 +0700

    Impl TachoMotorInterface for TachoMotor

--- src/motor/tacho/mod.rs
+++ src/motor/tacho/mod.rs
@@ -19,13 +19,13 @@ use crate::percentage::SignedPercentage;
 #[findable_device(class = "tacho-motor")]
 pub struct TachoMotor {
     command: WriteOnlyAttributeFile,
-    count_per_rot: u32,
+    count_per_rot: TachoCounts,
     duty_cycle: ReadOnlyAttributeFile,
     duty_cycle_sp: ReadWriteAttributeFile,
     polarity: ReadWriteAttributeFile,
     position: ReadWriteAttributeFile,
     position_sp: ReadWriteAttributeFile,
-    max_speed: u32,
+    max_speed: TachoCounts,
     state: ReadOnlyAttributeFile,
     speed: ReadOnlyAttributeFile,
     speed_sp: ReadWriteAttributeFile,
@@ -54,4 +54,81 @@ pub trait TachoMotorInterface {
     fn set_speed_sp(&mut self, value: TachoCounts) -> anyhow::Result<()>;
     fn stop_action(&mut self) -> anyhow::Result<StopAction>;
     fn set_stop_action(&mut self, value: StopAction) -> anyhow::Result<()>;
+}
+
+impl TachoMotorInterface for TachoMotor {
+    fn command(&mut self, value: Command) -> anyhow::Result<()> {
+        self.command.set_value(value).map_err(Into::into)
+    }
+
+    fn count_per_rot(&self) -> TachoCounts {
+        self.count_per_rot
+    }
+
+    fn duty_cycle(&mut self) -> anyhow::Result<SignedPercentage> {
+        self.duty_cycle.value().map_err(Into::into)
+    }
+
+    fn duty_cycle_sp(&mut self) -> anyhow::Result<SignedPercentage> {
+        self.duty_cycle_sp.value().map_err(Into::into)
+    }
+
+    fn set_duty_cycle_sp(
+        &mut self,
+        value: SignedPercentage,
+    ) -> anyhow::Result<()> {
+        self.duty_cycle_sp.set_value(value).map_err(Into::into)
+    }
+
+    fn polarity(&mut self) -> anyhow::Result<Polarity> {
+        self.polarity.value().map_err(Into::into)
+    }
+
+    fn set_polarity(&mut self, value: Polarity) -> anyhow::Result<()> {
+        self.polarity.set_value(value).map_err(Into::into)
+    }
+
+    fn position(&mut self) -> anyhow::Result<TachoCounts> {
+        self.position.value().map_err(Into::into)
+    }
+
+    fn set_position(&mut self, value: TachoCounts) -> anyhow::Result<()> {
+        self.position.set_value(value).map_err(Into::into)
+    }
+
+    fn position_sp(&mut self) -> anyhow::Result<TachoCounts> {
+        self.position_sp.value().map_err(Into::into)
+    }
+
+    fn set_position_sp(&mut self, value: TachoCounts) -> anyhow::Result<()> {
+        self.position_sp.set_value(value).map_err(Into::into)
+    }
+
+    fn max_speed(&self) -> TachoCounts {
+        self.max_speed
+    }
+
+    fn state(&mut self) -> anyhow::Result<State> {
+        self.state.value().map_err(Into::into)
+    }
+
+    fn speed(&mut self) -> anyhow::Result<TachoCounts> {
+        self.speed.value().map_err(Into::into)
+    }
+
+    fn speed_sp(&mut self) -> anyhow::Result<TachoCounts> {
+        self.speed_sp.value().map_err(Into::into)
+    }
+
+    fn set_speed_sp(&mut self, value: TachoCounts) -> anyhow::Result<()> {
+        self.speed_sp.set_value(value).map_err(Into::into)
+    }
+
+    fn stop_action(&mut self) -> anyhow::Result<StopAction> {
+        self.stop_action.value().map_err(Into::into)
+    }
+
+    fn set_stop_action(&mut self, value: StopAction) -> anyhow::Result<()> {
+        self.stop_action.set_value(value).map_err(Into::into)
+    }
 }
