c80f22b3e26b8c166cda2b0b73d0df3425df9f1f
Nikitat0 <nikitatzero@yandex.ru>
2022-08-19 17:42:36 +0700

    fixup! Add motor::tacho::TachoCounts

--- src/motor/tacho/tacho_counts.rs
+++ src/motor/tacho/tacho_counts.rs
@@ -1,28 +1,35 @@
-use std::fmt::{self, Debug, Display};
+use std::fmt::{self, Display};
 
 use derive_more::*;
 
 #[derive(
-    Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Constructor, From, FromStr,
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Constructor,
+    From,
+    FromStr,
 )]
-pub struct TachoCounts {
-    pub value: u32,
-}
+pub struct TachoCounts(pub u32);
 
-impl Debug for TachoCounts {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("TachoCounts").field(&self.value).finish()
+impl TachoCounts {
+    fn value(self) -> u32 {
+        self.into()
     }
 }
 
 impl Display for TachoCounts {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        Display::fmt(&self.value, f)
+        Display::fmt(&self.value(), f)
     }
 }
 
 impl From<TachoCounts> for u32 {
-    fn from(TachoCounts { value }: TachoCounts) -> Self {
+    fn from(TachoCounts(value): TachoCounts) -> Self {
         value
     }
 }
